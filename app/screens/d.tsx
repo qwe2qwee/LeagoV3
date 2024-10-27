e6d160cfb0042fcda9a36a6fa8275d8dca489cf1
qwe2qwe <qwe2qwe@hotmail.com>
2024-10-27 19:15:32 +0300

    new screens

---
+++ app/screens/d.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const d = () => {
+  return (
+    <View>
+      <Text>d</Text>
+    </View>
+  );
+};
+
+export default d;
